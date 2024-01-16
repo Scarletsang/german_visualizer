e093e9e183004ba34f0cabad4b3a390fb03171f9
Anthony Tsang <anthonytsang0226@gmail.com>
2024-01-16 01:31:54 +0100

    chore(Tokenizer/Markdown): Add punctuation characters as settings for the tokenizer

--- data/src/Markdown/Parser/Tokenizer/Markdown/Settings.cpp
+++ data/src/Markdown/Parser/Tokenizer/Markdown/Settings.cpp
@@ -2,22 +2,44 @@
 
 #include <string>
 
+const std::string  MarkdownTokenizerSettings::kDefaultSpaceCharacters = " \t\n\r\f\v";
+const std::string  MarkdownTokenizerSettings::kDefaultPunctuationCharacters = ".!?,;:()[]{}<>\"'`~@#$%^&*\\|/-_=+";
+
 MarkdownTokenizerSettings::MarkdownTokenizerSettings()
-  : space_characters_(" \t\n\r\f\v") {}
+  : space_characters_(kDefaultSpaceCharacters), punctuation_characters_(kDefaultPunctuationCharacters) {}
 
 MarkdownTokenizerSettings::MarkdownTokenizerSettings(const MarkdownTokenizerSettings& settings)
-  : space_characters_(settings.space_characters_) {}
+  : space_characters_(settings.space_characters_),
+    punctuation_characters_(settings.punctuation_characters_) {}
 
 MarkdownTokenizerSettings::~MarkdownTokenizerSettings() {}
 
 MarkdownTokenizerSettings& MarkdownTokenizerSettings::operator=(const MarkdownTokenizerSettings& settings)
 {
   if (this != &settings)
+  {
     space_characters_ = settings.space_characters_;
+    punctuation_characters_ = settings.punctuation_characters_;
+  }
   return *this;
 }
 
 bool  MarkdownTokenizerSettings::is_space(char character) const
 {
   return space_characters_.find(character) != std::string::npos;
+}
+
+bool  MarkdownTokenizerSettings::is_punctuation(char character) const
+{
+  return punctuation_characters_.find(character) != std::string::npos;
+}
+
+void  MarkdownTokenizerSettings::set_space_characters(const std::string& space_characters)
+{
+  space_characters_ = space_characters;
+}
+
+void  MarkdownTokenizerSettings::set_punctuation_characters(const std::string& punctuation_characters)
+{
+  punctuation_characters_ = punctuation_characters;
 }
--- data/src/Markdown/Parser/Tokenizer/Markdown/Settings.hpp
+++ data/src/Markdown/Parser/Tokenizer/Markdown/Settings.hpp
@@ -5,6 +5,8 @@
 class MarkdownTokenizerSettings
 {
   public:
+    static const std::string  kDefaultSpaceCharacters;
+    static const std::string  kDefaultPunctuationCharacters;
     MarkdownTokenizerSettings();
     MarkdownTokenizerSettings(const MarkdownTokenizerSettings& settings);
     ~MarkdownTokenizerSettings();
@@ -12,7 +14,12 @@ class MarkdownTokenizerSettings
     MarkdownTokenizerSettings& operator=(const MarkdownTokenizerSettings& settings);
 
     bool  is_space(char character) const;
+    bool  is_punctuation(char character) const;
+
+    void  set_space_characters(const std::string& space_characters);
+    void  set_punctuation_characters(const std::string& punctuation_characters);
 
   private:
     std::string space_characters_;
+    std::string punctuation_characters_;
 };
