0e6353f5af5300d233198251c6bfa290b40cde70
Anthony Tsang <anthonytsang0226@gmail.com>
2024-02-02 00:46:08 +0100

    feat(core): Add bindings. Untested

---
+++ core/bindings/Ast.cpp
@@ -0,0 +1,115 @@
+#include "Ast/Type.hpp"
+#include "Ast/Element.hpp"
+#include "Ast/Atom/Character.hpp"
+#include "Ast/Atom/Delimiter.hpp"
+#include "Ast/Atom/Word.hpp"
+#include "Ast/Atom/Number.hpp"
+#include "Ast/DomCollection/Title.hpp"
+#include "Ast/DomCollection/Sentence.hpp"
+#include "Ast/DomCollection/Paragraph.hpp"
+#include "Ast/DomCollection/Section.hpp"
+#include "Ast/DomCollection/Document.hpp"
+#include "Ast/GraphCollection/Edge.hpp"
+#include "Ast/GraphCollection/Edge/Directed.hpp"
+#include "Ast/GraphCollection/Edge/Undirected.hpp"
+#include "Ast/GraphCollection/Tagged/Weighted.hpp"
+
+#include <emscripten/bind.h>
+
+using namespace emscripten;
+
+EMSCRIPTEN_BINDINGS(ast) {
+
+    enum_<ElementType>("AstType")
+        .value("kAtom", ElementType::kAtom)
+        .value("kDomCollection", ElementType::kDomCollection)
+        .value("kGraphCollection", ElementType::kGraphCollection);
+
+    class_<MarkdownElement>("AstElement")
+        .function("encode", &MarkdownElement::encode, pure_virtual())
+        .function("type", &MarkdownElement::type, pure_virtual());
+
+    class_<Character, base<MarkdownElement>>("AstCharacter")
+        .constructor<char>()
+        .constructor<const Character&>()
+        .function("encode", &Character::encode)
+        .function("type", &Character::type)
+        .function("data", select_const(&Character::data), allow_raw_pointers())
+        .function("writable_data", select_overload<char&()>(&Character::data), allow_raw_pointers());
+    
+    class_<Delimiter, base<MarkdownElement>>("AstDelimiter")
+        .constructor<const std::string&>()
+        .constructor<const Delimiter&>()
+        .function("encode", &Delimiter::encode)
+        .function("type", &Delimiter::type)
+        .function("data", select_const(&Delimiter::data), allow_raw_pointers())
+        .function("writable_data", select_overload<std::string&()>(&Delimiter::data), allow_raw_pointers());
+
+    class_<Word, base<MarkdownElement>>("AstWord")
+        .constructor<const std::string&>()
+        .constructor<const Word&>()
+        .function("encode", &Word::encode)
+        .function("type", &Word::type)
+        .function("data", select_const(&Word::data), allow_raw_pointers())
+        .function("writable_data", select_overload<std::string&()>(&Word::data), allow_raw_pointers());
+
+    class_<atom::Number>("Number")
+        .constructor<int>()
+        .constructor<float>()
+        .constructor<const atom::Number&>()
+        .function("as_int", &atom::Number::get_integer)
+        .function("as_float", &atom::Number::get_float);
+
+    class_<Number, base<MarkdownElement>>("AstNumber")
+        .constructor<float>()
+        .constructor<const Number&>()
+        .function("encode", &Number::encode)
+        .function("type", &Number::type)
+        .function("data", select_const(&Number::data), allow_raw_pointers())
+        .function("writable_data", select_overload<atom::Number&()>(&Number::data), allow_raw_pointers());
+
+    class_<Title, base<MarkdownElement>>("AstTitle")
+        .constructor<int>()
+        .constructor<int, std::shared_ptr<Paragraph>>()
+        .constructor<const Title&>()
+        .function("encode", &Title::encode)
+        .function("type", &Title::type)
+        .property("data", &Title::data, &Title::set_data)
+        .property("level", &Title::level, &Title::set_level);
+
+    class_<Paragraph, base<MarkdownElement>>("AstParagraph")
+        .constructor<const Paragraph&>()
+        .function("encode", &Paragraph::encode)
+        .function("type", &Paragraph::type)
+        .function("add_element", select_overload<int(std::shared_ptr<Sentence>)>(&Paragraph::add_element))
+        .function("add_elements", &Paragraph::add_elements)
+        .function("data", select_const(&Paragraph::data), allow_raw_pointers())
+        .function("writable_data", select_overload<std::vector<std::shared_ptr<Sentence>>&()>(&Paragraph::data), allow_raw_pointers());
+
+    using StandardDomCollection = DomCollection<MarkdownElement>;
+    class_<StandardDomCollection, base<MarkdownElement>>("AstDomCollection")
+        .constructor<const StandardDomCollection&>() 
+        .function("add_element", select_overload<int(std::shared_ptr<MarkdownElement>)>(&StandardDomCollection::add_element))
+        .function("add_elements", &StandardDomCollection::add_elements)
+        .function("data", select_const(&StandardDomCollection::data), allow_raw_pointers())
+        .function("writable_data", select_overload<std::vector<std::shared_ptr<MarkdownElement>>&()>(&StandardDomCollection::data), allow_raw_pointers());
+
+    class_<Sentence, base<StandardDomCollection>>("AstSentence")
+        .constructor<const Sentence&>()
+        .function("encode", &Sentence::encode)
+        .function("type", &Sentence::type)
+        .function("add_element", select_overload<int(std::shared_ptr<MarkdownElement>)>(&Sentence::add_element));
+
+    class_<Section, base<StandardDomCollection>>("AstSection")
+        .constructor<std::shared_ptr<Title>>()
+        .constructor<const Section&>()
+        .function("encode", &Section::encode)
+        .function("type", &Section::type)
+        .property("title", &Section::title, &Section::set_title);
+
+    class_<Document, base<StandardDomCollection>>("AstDocument")
+        .constructor<const Document&>()
+        .function("encode", &Document::encode)
+        .function("type", &Document::type);
+}
+
---
+++ core/bindings/Encoder.cpp
@@ -0,0 +1,25 @@
+#include "Encoder.hpp"
+
+#include <emscripten/bind.h>
+
+#include "Ast.hpp"
+
+using namespace emscripten;
+
+EMSCRIPTEN_BINDINGS(encoder) {
+    class_<Encoder, base<Encoder>>("Encoder")
+        .function("encode_character", select_overload<int(const Character&)>(&Encoder::encode), pure_virtual())
+        .function("encode_word", select_overload<int(const Word&)>(&Encoder::encode), pure_virtual())
+        .function("encode_delimiter", select_overload<int(const Delimiter&)>(&Encoder::encode), pure_virtual())
+        .function("encode_number", select_overload<int(const Number&)>(&Encoder::encode), pure_virtual())
+        .function("encode_title", select_overload<int(const Title&)>(&Encoder::encode), pure_virtual())
+        .function("encode_sentence", select_overload<int(const Sentence&)>(&Encoder::encode), pure_virtual())
+        .function("encode_paragraph", select_overload<int(const Paragraph&)>(&Encoder::encode), pure_virtual())
+        .function("encode_section", select_overload<int(const Section&)>(&Encoder::encode), pure_virtual())
+        .function("encode_Document", select_overload<int(const Document&)>(&Encoder::encode), pure_virtual())
+        .function("encode_directed_edge", select_overload<int(const DirectedEdge&)>(&Encoder::encode), pure_virtual())
+        .function("encode_undirected_edge", select_overload<int(const UndirectedEdge&)>(&Encoder::encode), pure_virtual())
+        .function("encode_weighted_edge", select_overload<int(const WeightedEdge&)>(&Encoder::encode), pure_virtual())
+        .function("encode_graph", select_overload<int(const Graph&)>(&Encoder::encode), pure_virtual())
+        .function("encode_element", select_overload<int(const MarkdownElement&)>(&Encoder::encode), pure_virtual()); 
+}
