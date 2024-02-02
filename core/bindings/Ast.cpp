d2c6c7ff97dda1c1a8fe728e9532d47068cfbb2d
Anthony Tsang <anthonytsang0226@gmail.com>
2024-02-02 13:27:52 +0100

    fix(core/bindings): Add default constructor for all AST object. Allow inheritance from js of abstract class/interface

--- core/bindings/Ast.cpp
+++ core/bindings/Ast.cpp
@@ -1,3 +1,5 @@
+#include <emscripten/bind.h>
+
 #include "Ast/Type.hpp"
 #include "Ast/Element.hpp"
 #include "Ast/Atom/Character.hpp"
@@ -14,12 +16,44 @@
 #include "Ast/GraphCollection/Edge/Undirected.hpp"
 #include "Ast/GraphCollection/Tagged/Weighted.hpp"
 
-#include <emscripten/bind.h>
-
 using namespace emscripten;
 
-EMSCRIPTEN_BINDINGS(ast) {
+class MarkdownElementWrapper : public wrapper<MarkdownElement>
+{
+  public:
+    EMSCRIPTEN_WRAPPER(MarkdownElementWrapper);
 
+    virtual int encode(Encoder&) const override
+    {
+      return EXIT_FAILURE;
+    }
+
+    virtual ElementType type() const override
+    {
+      return call<ElementType>("type");
+    }
+};
+
+using StandardDomCollection = DomCollection<MarkdownElement>;
+
+class StandardDomCollectionWrapper : public wrapper<StandardDomCollection>
+{
+  public:
+    EMSCRIPTEN_WRAPPER(StandardDomCollectionWrapper);
+
+    virtual int encode(Encoder&) const override
+    {
+      return EXIT_FAILURE;
+    }
+
+    virtual ElementType type() const override
+    {
+      return call<ElementType>("type");
+    }
+};
+
+EMSCRIPTEN_BINDINGS(ast)
+{
     enum_<ElementType>("AstType")
         .value("kAtom", ElementType::kAtom)
         .value("kDomCollection", ElementType::kDomCollection)
@@ -27,16 +61,19 @@ EMSCRIPTEN_BINDINGS(ast) {
 
     class_<MarkdownElement>("AstElement")
         .function("encode", &MarkdownElement::encode, pure_virtual())
-        .function("type", &MarkdownElement::type, pure_virtual());
+        .function("type", &MarkdownElement::type, pure_virtual())
+        .allow_subclass<MarkdownElementWrapper>("MarkdownElementWrapper");
 
     class_<Character, base<MarkdownElement>>("AstCharacter")
+        .constructor<>()
         .constructor<char>()
         .function("encode", &Character::encode)
         .function("type", &Character::type)
-        .function("data", select_const(&Character::data), allow_raw_pointers())
-        .function("writable_data", select_overload<char&()>(&Character::data), allow_raw_pointers());
+        .function("data", select_overload<const char&() const>(&Character::data))
+        .function("writable_data", select_overload<char&()>(&Character::data));
     
     class_<Delimiter, base<MarkdownElement>>("AstDelimiter")
+        .constructor<>()
         .constructor<const std::string&>()
         .function("encode", &Delimiter::encode)
         .function("type", &Delimiter::type)
@@ -44,6 +81,7 @@ EMSCRIPTEN_BINDINGS(ast) {
         .function("writable_data", select_overload<std::string&()>(&Delimiter::data), allow_raw_pointers());
 
     class_<Word, base<MarkdownElement>>("AstWord")
+        .constructor<>()
         .constructor<const std::string&>()
         .function("encode", &Word::encode)
         .function("type", &Word::type)
@@ -51,12 +89,14 @@ EMSCRIPTEN_BINDINGS(ast) {
         .function("writable_data", select_overload<std::string&()>(&Word::data), allow_raw_pointers());
 
     class_<atom::Number>("Number")
+        .constructor<>()
         .function("set_integer", &atom::Number::set_integer)
         .function("set_float", &atom::Number::set_float)
         .function("as_integer", &atom::Number::get_integer)
         .function("as_float", &atom::Number::get_float);
 
     class_<Number, base<MarkdownElement>>("AstNumber")
+        .constructor<>()
         .constructor<atom::Number>()
         .function("encode", &Number::encode)
         .function("type", &Number::type)
@@ -64,6 +104,7 @@ EMSCRIPTEN_BINDINGS(ast) {
         .function("writable_data", select_overload<atom::Number&()>(&Number::data), allow_raw_pointers());
 
     class_<Title, base<MarkdownElement>>("AstTitle")
+        .constructor<>()
         .constructor<int>()
         .constructor<int, std::shared_ptr<Paragraph>>()
         .function("encode", &Title::encode)
@@ -72,6 +113,7 @@ EMSCRIPTEN_BINDINGS(ast) {
         .property("level", &Title::level, &Title::set_level);
 
     class_<Paragraph, base<MarkdownElement>>("AstParagraph")
+        .constructor<>()
         .function("encode", &Paragraph::encode)
         .function("type", &Paragraph::type)
         .function("add_element", select_overload<int(std::shared_ptr<Sentence>)>(&Paragraph::add_element))
@@ -79,25 +121,31 @@ EMSCRIPTEN_BINDINGS(ast) {
         .function("data", select_const(&Paragraph::data), allow_raw_pointers())
         .function("writable_data", select_overload<std::vector<std::shared_ptr<Sentence>>&()>(&Paragraph::data), allow_raw_pointers());
 
-    using StandardDomCollection = DomCollection<MarkdownElement>;
     class_<StandardDomCollection, base<MarkdownElement>>("AstDomCollection")
+        .constructor<>()
+        .function("encode", &StandardDomCollection::encode)
+        .function("type", &StandardDomCollection::type)
         .function("add_element", select_overload<int(std::shared_ptr<MarkdownElement>)>(&StandardDomCollection::add_element))
         .function("add_elements", &StandardDomCollection::add_elements)
         .function("data", select_const(&StandardDomCollection::data), allow_raw_pointers())
-        .function("writable_data", select_overload<std::vector<std::shared_ptr<MarkdownElement>>&()>(&StandardDomCollection::data), allow_raw_pointers());
+        .function("writable_data", select_overload<std::vector<std::shared_ptr<MarkdownElement>>&()>(&StandardDomCollection::data), allow_raw_pointers())
+        .allow_subclass<StandardDomCollectionWrapper>("StandardDomCollectionWrapper");
 
     class_<Sentence, base<StandardDomCollection>>("AstSentence")
+        .constructor<>()
         .function("encode", &Sentence::encode)
         .function("type", &Sentence::type)
         .function("add_element", select_overload<int(std::shared_ptr<MarkdownElement>)>(&Sentence::add_element));
 
     class_<Section, base<StandardDomCollection>>("AstSection")
+        .constructor<>()
         .constructor<std::shared_ptr<Title>>()
         .function("encode", &Section::encode)
         .function("type", &Section::type)
         .property("title", &Section::title, &Section::set_title);
 
     class_<Document, base<StandardDomCollection>>("AstDocument")
+        .constructor<>()
         .function("encode", &Document::encode)
         .function("type", &Document::type);
 }
--- core/bindings/Encoder.cpp
+++ core/bindings/Encoder.cpp
@@ -2,11 +2,103 @@
 
 #include <emscripten/bind.h>
 
-#include "Ast.hpp"
+#include "Ast/Type.hpp"
+#include "Ast/Element.hpp"
+#include "Ast/Atom/Character.hpp"
+#include "Ast/Atom/Delimiter.hpp"
+#include "Ast/Atom/Word.hpp"
+#include "Ast/Atom/Number.hpp"
+#include "Ast/DomCollection/Title.hpp"
+#include "Ast/DomCollection/Sentence.hpp"
+#include "Ast/DomCollection/Paragraph.hpp"
+#include "Ast/DomCollection/Section.hpp"
+#include "Ast/DomCollection/Document.hpp"
+#include "Ast/GraphCollection/Edge.hpp"
+#include "Ast/GraphCollection/Edge/Directed.hpp"
+#include "Ast/GraphCollection/Edge/Undirected.hpp"
+#include "Ast/GraphCollection/Tagged/Weighted.hpp"
+#include "Ast/Graph.hpp"
 
 using namespace emscripten;
 
-EMSCRIPTEN_BINDINGS(encoder) {
+class EncoderWrapper : public wrapper<Encoder>
+{
+  public:
+    EMSCRIPTEN_WRAPPER(EncoderWrapper);
+
+    int encode(const Character& character) override
+    {
+      return call<int>("encode", character);
+    }
+
+    int encode(const Word& word) override
+    {
+      return call<int>("encode", word);
+    }
+
+    int encode(const Delimiter& delimiter) override
+    {
+      return call<int>("encode", delimiter);
+    }
+
+    int encode(const Number& number) override
+    {
+      return call<int>("encode", number);
+    }
+
+    int encode(const Title& title) override
+    {
+      return call<int>("encode", title);
+    }
+
+    int encode(const Sentence& sentence) override
+    {
+      return call<int>("encode", sentence);
+    }
+
+    int encode(const Paragraph& paragraph) override
+    {
+      return call<int>("encode", paragraph);
+    }
+
+    int encode(const Section& section) override
+    {
+      return call<int>("encode", section);
+    }
+
+    int encode(const Document& document) override
+    {
+      return call<int>("encode", document);
+    }
+
+    int encode(const DirectedEdge& directed_edge) override
+    {
+      return call<int>("encode", directed_edge);
+    }
+
+    int encode(const UndirectedEdge& undirected_edge) override
+    {
+      return call<int>("encode", undirected_edge);
+    }
+
+    int encode(const WeightedEdge& weighted_edge) override
+    {
+      return call<int>("encode", weighted_edge);
+    }
+
+    int encode(const Graph& graph) override
+    {
+      return call<int>("encode", graph);
+    }
+
+    int encode(const MarkdownElement&) override
+    {
+      return EXIT_FAILURE;
+    }
+};
+
+EMSCRIPTEN_BINDINGS(encoder)
+{
     class_<Encoder>("Encoder")
         .function("encode_character", select_overload<int(const Character&)>(&Encoder::encode), pure_virtual())
         .function("encode_word", select_overload<int(const Word&)>(&Encoder::encode), pure_virtual())
@@ -21,5 +113,6 @@ EMSCRIPTEN_BINDINGS(encoder) {
         .function("encode_undirected_edge", select_overload<int(const UndirectedEdge&)>(&Encoder::encode), pure_virtual())
         .function("encode_weighted_edge", select_overload<int(const WeightedEdge&)>(&Encoder::encode), pure_virtual())
         .function("encode_graph", select_overload<int(const Graph&)>(&Encoder::encode), pure_virtual())
-        .function("encode_element", select_overload<int(const MarkdownElement&)>(&Encoder::encode), pure_virtual()); 
+        .function("encode_element", select_overload<int(const MarkdownElement&)>(&Encoder::encode), pure_virtual())
+        .allow_subclass<EncoderWrapper>("EncoderWrapper");
 }
